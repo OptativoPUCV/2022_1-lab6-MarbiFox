db5d55d131b7f5521f2503c8ef6e13ce1eaa93e1
MarbiFox <>
2022-05-14 02:22:49 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -54,7 +54,7 @@ int is_valid(Node* n){
         int i = 3 * (k/3) + (p/3) ;
         int j = 3 * (k%3) + (p%3) ;
         printf("%d ",n->sudo[i][j]);
-        if(p%3 == 2) printf("\n");
+        if(p%3 == 2) //printf("\n");
       }
   }
   
