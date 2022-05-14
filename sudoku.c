43bec2d4094dee50b9280c996cce4841fd100662
MarbiFox <>
2022-05-14 02:47:45 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -117,8 +117,8 @@ Node* DFS(Node* initial, int* cont){
   Stack * S = createStack();
   push(S, initial);
   while (get_size(S) > 0) {
-    cont++;
     Node * n = top(S); pop(S);
+    if (n == NULL) return NULL;
     if (is_final(n)) return n;
     List * adj = get_adj_nodes(n);
     Node * aux = first(adj);
